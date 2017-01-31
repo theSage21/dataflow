4e94022cec387cec0eb9a631cbdf52fb6799799c
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-01-31 09:57:24 +0530

    bootstrapped

--- dataflow.py
+++ dataflow.py
@@ -1,9 +1,20 @@
 import os
 import json
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+# from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from bottle import get, run, post, static_file, request
 
 
+#########################################################################
+# FLOW generator
+#########################################################################
+
+def to_code(chart):
+    """convert chart to code"""
+
+
+#########################################################################
+# INTERFACE
+#########################################################################
 @get('/')
 def main_page():
     scripts = ''
@@ -14,10 +25,11 @@ def main_page():
 
 @get('/scripts/')
 def scritlist():
-    scripts = ['<li><a href=/static/scripts/{}>{}</a></li>'.format(i, i) for i in os.listdir('static/scripts')]
+    scripts = ['<tr><td><a href=/static/scripts/{}>{}</td></tr>'.format(i, i) for i in os.listdir('static/scripts')]
     scripts = '\n'.join(scripts)
-    scripts = '<ul>'+scripts+'</ul>'
-    html = '<html><body><h1>Scripts</h1>{}</body></html>'.format(scripts)
+    with open('scripts.html', 'r') as fl:
+        html = fl.read()
+        html = html.replace('<Areafortablerows>', scripts)
     return html
 
 @post('/makescript')
@@ -35,4 +47,5 @@ def makescript():
 @get("/static/<filepath:path>")
 def css(filepath):
     return static_file(filepath, root="/home/arjoonn/dev/dataflow/static")
-run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080, debug=True)
